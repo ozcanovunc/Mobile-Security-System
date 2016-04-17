f0ea5cebf3c9c10f49d24796a82d209edeb329ad
ozcanovunc <ozcan_ovunc@hotmail.com>
2016-04-17 23:04:15 +0300

    Converting image to ARGB works now

---
+++ Mobile Security System/Image.cs
@@ -0,0 +1,67 @@
+﻿namespace Mobile_Security_System
+{
+    public class Image
+    {
+        private int[] a_;
+        private int[] r_;
+        private int[] g_;
+        private int[] b_;
+
+        private int width_;
+        private int height_;
+
+        public Image(int[] image, int width, int height)
+        {
+            width_ = width;
+            height_ = height;
+
+            a_ = new int[image.Length];
+            r_ = new int[image.Length];
+            g_ = new int[image.Length];
+            b_ = new int[image.Length];
+
+            // Initialize A, R, G and B values for each pixel
+            for (int pi = 0; pi < image.Length; ++pi)
+            {
+                GetARGB(image[pi], out a_[pi], out r_[pi], out g_[pi], out b_[pi]);
+            }
+        }
+
+        public int[] GetA()
+        {
+            return a_;
+        }
+
+        public int[] GetR()
+        {
+            return r_;
+        }
+
+        public int[] GetG()
+        {
+            return g_;
+        }
+
+        public int[] GetB()
+        {
+            return b_;
+        }
+
+        private static void GetARGB(int color, out int a, out int r, out int g, 
+            out int b)
+        {
+            a = color >> 24;
+            r = (color & 0x00ff0000) >> 16;
+            g = (color & 0x0000ff00) >> 8;
+            b = (color & 0x000000ff);
+        }
+
+        private static int GetColorFromArgb(int a, int r, int g, int b)
+        {
+            int result = ((a & 0xFF) << 24) | ((r & 0xFF) << 16) | 
+                ((g & 0xFF) << 8) | (b & 0xFF);
+
+            return result;
+        }
+    }
+}
--- Mobile Security System/MainPage.xaml.cs
+++ Mobile Security System/MainPage.xaml.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.Devices;
 using Microsoft.Phone.Controls;
-using Microsoft.Xna.Framework.Media;
 using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Navigation;
 
@@ -11,13 +10,11 @@ namespace Mobile_Security_System
     public partial class MainPage : PhoneApplicationPage
     {
         private PhotoCamera camera_;
-        private MediaLibrary library_ = new MediaLibrary(); // TODO: Remove
-        private bool cameraAvailable_;
+        private Size cameraRes_;
 
         public MainPage()
         {
             InitializeComponent();
-            cameraAvailable_ = false;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -27,18 +24,12 @@ namespace Mobile_Security_System
 
             // Event is fired when the PhotoCamera object has been initialized.
             camera_.Initialized += new EventHandler<CameraOperationCompletedEventArgs>(cam_Initialized);
-            // Event is fired when the capture sequence is complete.
-            camera_.CaptureCompleted += new EventHandler<CameraOperationCompletedEventArgs>(cam_CaptureCompleted);
-            // Event is fired when the capture sequence is complete and an image is available.
-            camera_.CaptureImageAvailable += new EventHandler<ContentReadyEventArgs>(cam_CaptureImageAvailable);
         }
 
         protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
         {
             camera_.Dispose();
             camera_.Initialized -= cam_Initialized;
-            camera_.CaptureCompleted -= cam_CaptureCompleted;
-            camera_.CaptureImageAvailable -= cam_CaptureImageAvailable;
         }
 
         private void cam_Initialized(object sender, CameraOperationCompletedEventArgs e)
@@ -47,48 +38,18 @@ namespace Mobile_Security_System
             if (camera_.IsFlashModeSupported(FlashMode.On))
                 camera_.FlashMode = FlashMode.Off;
 
-            cameraAvailable_ = true;
-        }
-
-        private void cam_CaptureCompleted(object sender, CameraOperationCompletedEventArgs e)
-        {
-            cameraAvailable_ = true;
-        }
-
-        private void cam_CaptureImageAvailable(object sender, ContentReadyEventArgs e)
-        {
-            library_.SavePictureToCameraRoll("x.jpg", e.ImageStream); // TODO: Remove
-            byte[] image = streamToByteArray(e.ImageStream);
-
-
-
-            e.ImageStream.Close();
+            // Setting the resolution of the camera
+            IEnumerator<Size> resList = camera_.AvailableResolutions.GetEnumerator();
+            resList.MoveNext();
+            cameraRes_ = camera_.Resolution = resList.Current;
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            if (cameraAvailable_)
-            {
-                camera_.CaptureImage();
-                cameraAvailable_ = false;
-            }
-        }
+            int[] ARGBPx = new int[(int)cameraRes_.Width * (int)cameraRes_.Height];
+            camera_.GetPreviewBufferArgb32(ARGBPx);
 
-        private static byte[] streamToByteArray(Stream stream)
-        {
-            byte[] buffer = new byte[4096]; // 4KB
-            int read;
-
-            stream.Seek(0, SeekOrigin.Begin);
-
-            using (MemoryStream ms = new MemoryStream())
-            {
-                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    ms.Write(buffer, 0, read);
-                }
-                return ms.ToArray();
-            }
+            Image image = new Image(ARGBPx, (int)cameraRes_.Width, (int)cameraRes_.Height);
         }
     }
 }
