ae5b663b2bd8c368fd9442227b7e0e831d1891af
ozcanovunc <ozcan_ovunc@hotmail.com>
2016-04-29 00:47:28 +0300

    Updated motion detection

--- Mobile Security System/MainPage.xaml.cs
+++ Mobile Security System/MainPage.xaml.cs
@@ -2,8 +2,10 @@
 using Microsoft.Phone.Controls;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Navigation;
+using System.Windows.Threading;
 
 namespace Mobile_Security_System
 {
@@ -11,10 +13,21 @@ namespace Mobile_Security_System
     {
         private PhotoCamera camera_;
         private Size cameraRes_;
+        private bool cameraInitialized_;
+        private Image firstImageToCompare_;
+        private readonly DispatcherTimer timer_;
 
         public MainPage()
         {
             InitializeComponent();
+
+            cameraInitialized_ = false;
+
+            // Start the periodic event for motion detection
+            timer_ = new DispatcherTimer();
+            timer_.Interval = TimeSpan.FromMilliseconds(250);
+            timer_.Tick += (o, arg) => ScanPreviewBuffer();
+            timer_.Start();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -42,14 +55,60 @@ namespace Mobile_Security_System
             IEnumerator<Size> resList = camera_.AvailableResolutions.GetEnumerator();
             resList.MoveNext();
             cameraRes_ = camera_.Resolution = resList.Current;
+
+            cameraInitialized_ = true;
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void ScanPreviewBuffer()
         {
-            int[] ARGBPx = new int[(int)cameraRes_.Width * (int)cameraRes_.Height];
-            camera_.GetPreviewBufferArgb32(ARGBPx);
+            if (cameraInitialized_)
+            {
+                int[] ARGBPx = new int[(int)cameraRes_.Width * (int)cameraRes_.Height];
+                camera_.GetPreviewBufferArgb32(ARGBPx);
 
-            Image image = new Image(ARGBPx, (int)cameraRes_.Width, (int)cameraRes_.Height);
+                if (firstImageToCompare_ == null)
+                {
+                    firstImageToCompare_ = new Image(ARGBPx, (int)cameraRes_.Width, (int)cameraRes_.Height);
+                }
+                // Do the comparison and detect the motion
+                else
+                {
+                    Image newImage = new Image(ARGBPx, (int)cameraRes_.Width, (int)cameraRes_.Height);
+                    int[] r1 = firstImageToCompare_.GetR();
+                    int[] r2 = newImage.GetR();
+
+                    // Update the screen if any motion has been detected
+                    if (DetectMotion(firstImageToCompare_, newImage))
+                    {
+                        textBlock.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        textBlock.Visibility = Visibility.Collapsed;
+                    }
+
+                    // Update the image
+                    firstImageToCompare_ = newImage;
+                }
+            }
+
+        }
+
+        private static bool DetectMotion(Image rhs, Image lhs)
+        {
+            int[] rValuesRhs = rhs.GetR();
+            int[] rValuesLhs = lhs.GetR();
+            int difference = 0;
+
+            for (int i = 0; i < rValuesRhs.Length; ++i)
+            {
+                if (rValuesRhs[i] - rValuesLhs[i] > 50)
+                {
+                    ++difference;
+                }
+            }
+
+            return difference > 20000;
         }
     }
 }
