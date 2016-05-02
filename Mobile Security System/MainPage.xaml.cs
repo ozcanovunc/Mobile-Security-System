9cb3813d97296c64ab8a6ec7d5172b637797d44a
ozcanovunc <ozcan_ovunc@hotmail.com>
2016-05-02 23:52:02 +0300

    Implemented flashlight support

--- Mobile Security System/MainPage.xaml.cs
+++ Mobile Security System/MainPage.xaml.cs
@@ -15,7 +15,15 @@ namespace Mobile_Security_System
         private Size cameraRes_;
         private bool cameraInitialized_;
         private Image firstImageToCompare_;
-        private readonly DispatcherTimer timer_;
+        private readonly DispatcherTimer cameraTimer_, flashTimer_;
+
+        // Hour thresholds for deciding if we should activate the flash or not
+        private const int NightHourStarting = 18;
+        private const int NightHourEnding = 6;
+
+        // If the number of different pixels of two images is greater than this, 
+        // they are not equal
+        private const int NumOfDifferentPixels = 10000;
 
         public MainPage()
         {
@@ -24,10 +32,16 @@ namespace Mobile_Security_System
             cameraInitialized_ = false;
 
             // Start the periodic event for motion detection
-            timer_ = new DispatcherTimer();
-            timer_.Interval = TimeSpan.FromMilliseconds(250);
-            timer_.Tick += (o, arg) => ScanPreviewBuffer();
-            timer_.Start();
+            cameraTimer_ = new DispatcherTimer();
+            cameraTimer_.Interval = TimeSpan.FromMilliseconds(250);
+            cameraTimer_.Tick += (o, arg) => ScanPreviewBuffer();
+            cameraTimer_.Start();
+
+            // Start the periodic event for deciding if we keep the flash on or off
+            flashTimer_ = new DispatcherTimer();
+            flashTimer_.Interval = TimeSpan.FromHours(1);
+            flashTimer_.Tick += (o, arg) => UpdateFlashState();
+            flashTimer_.Start();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -36,7 +50,8 @@ namespace Mobile_Security_System
             videoBrush.SetSource(camera_);
 
             // Event is fired when the PhotoCamera object has been initialized.
-            camera_.Initialized += new EventHandler<CameraOperationCompletedEventArgs>(cam_Initialized);
+            camera_.Initialized += 
+                new EventHandler<CameraOperationCompletedEventArgs>(cam_Initialized);
         }
 
         protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
@@ -49,7 +64,9 @@ namespace Mobile_Security_System
         {
             // Turn off flash
             if (camera_.IsFlashModeSupported(FlashMode.On))
+            {
                 camera_.FlashMode = FlashMode.Off;
+            }
 
             // Setting the resolution of the camera
             IEnumerator<Size> resList = camera_.AvailableResolutions.GetEnumerator();
@@ -57,6 +74,7 @@ namespace Mobile_Security_System
             cameraRes_ = camera_.Resolution = resList.Current;
 
             cameraInitialized_ = true;
+            UpdateFlashState();
         }
 
         private void ScanPreviewBuffer()
@@ -64,16 +82,21 @@ namespace Mobile_Security_System
             if (cameraInitialized_)
             {
                 int[] ARGBPx = new int[(int)cameraRes_.Width * (int)cameraRes_.Height];
+
+                // Get the current frame
+                camera_.Focus();
                 camera_.GetPreviewBufferArgb32(ARGBPx);
 
                 if (firstImageToCompare_ == null)
                 {
-                    firstImageToCompare_ = new Image(ARGBPx, (int)cameraRes_.Width, (int)cameraRes_.Height);
+                    firstImageToCompare_ = 
+                        new Image(ARGBPx, (int)cameraRes_.Width, (int)cameraRes_.Height);
                 }
                 // Do the comparison and detect the motion
                 else
                 {
-                    Image newImage = new Image(ARGBPx, (int)cameraRes_.Width, (int)cameraRes_.Height);
+                    Image newImage = 
+                        new Image(ARGBPx, (int)cameraRes_.Width, (int)cameraRes_.Height);
                     int[] r1 = firstImageToCompare_.GetR();
                     int[] r2 = newImage.GetR();
 
@@ -91,9 +114,26 @@ namespace Mobile_Security_System
                     firstImageToCompare_ = newImage;
                 }
             }
-
         }
 
+        // Turn on or off the flash by hour of the day 
+        private void UpdateFlashState()
+        {
+            if (cameraInitialized_ && camera_.IsFlashModeSupported(FlashMode.On))
+            {
+                if (DateTime.Now.Hour > NightHourStarting || 
+                    DateTime.Now.Hour < NightHourEnding)
+                {
+                    camera_.FlashMode = FlashMode.On;
+                }
+                else
+                {
+                    camera_.FlashMode = FlashMode.Off;
+                }
+            }
+        }
+
+        // Returns true if there is a difference between rhs and lhs
         private static bool DetectMotion(Image rhs, Image lhs)
         {
             int[] rValuesRhs = rhs.GetR();
@@ -108,7 +148,23 @@ namespace Mobile_Security_System
                 }
             }
 
-            return difference > 20000;
+            return difference > NumOfDifferentPixels;
+        }
+
+        // If flash is activated, then deactivate it (or vice versa)
+        private void FlashlightClicked(object sender, EventArgs e)
+        {
+            if (cameraInitialized_ && camera_.IsFlashModeSupported(FlashMode.On))
+            {
+                if (camera_.FlashMode == FlashMode.On)
+                {
+                    camera_.FlashMode = FlashMode.Off;
+                }
+                else if (camera_.FlashMode == FlashMode.Off)
+                {
+                    camera_.FlashMode = FlashMode.On;
+                }
+            }
         }
     }
 }
